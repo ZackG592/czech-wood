190be5b9ae9132af47772431e8948388407b0748
ZackG592 <synderennn@gmail.com>
2025-04-09 12:56:58 +0200

    Create entities/wood section/WoodWeUseSection. Done Gallery page

--- src/entities/wood/Wood.tsx
+++ src/entities/wood/Wood.tsx
@@ -1,14 +1,64 @@
-import { FC } from "react"
+import { FC } from 'react';
+import Image from 'next/image';
 
-interface Wood {
-
+interface WoodStatistics {
+  name: string;
+  isTrue: boolean;
 }
 
-const Wood:FC<Wood> = () => {
-    return(
-        <>
-        </>
-    )
+interface WoodProps {
+  image: string;
+  name: string;
+  statistics: WoodStatistics[];
 }
 
-export default Wood
+const Wood: FC<WoodProps> = ({ name, image, statistics }) => {
+  return (
+    <div className="text-whites w-[100px] md:w-[125px]">
+      <div className="w-[100%]">
+        <div className="flex justify-center w-[100%]">
+          <Image
+            className="w-[100%] md:h-[125px]"
+            src={image}
+            alt="image"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="pt-2 md:pt-5 text-center text-[24px] md:text-[36px]">
+          {name}
+        </div>
+      </div>
+      <div>
+        {statistics.map((woodStatistics, index) => (
+          <div key={index} className="flex pt-2">
+            <div className="flex items-center mr-2">
+              {(woodStatistics.isTrue && (
+                <Image
+                  width={10}
+                  height={10}
+                  className="md:w-[15px] md:h-[15px]"
+                  src={'/icons/check-mark.svg'}
+                  alt="check-mark"
+                />
+              )) || (
+                <Image
+                  width={10}
+                  height={10}
+                  className="md:w-[15px] md:h-[15px]"
+                  src={'/icons/golden-cross.svg'}
+                  alt="golden cross"
+                />
+              )}
+            </div>
+            <div className="text-[10px] md:text-[20px] ml-2">
+              {woodStatistics.name}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Wood;
--- src/sections/woodWeUseSection/woodWeUseSection.tsx
+++ src/sections/woodWeUseSection/woodWeUseSection.tsx
@@ -1,8 +1,25 @@
+import Wood from '@/entities/wood/Wood';
 
+import mockData from './mockData';
 
-export default function WoodWeUseSection(){
-    return(
-        <>
-        </>
-    )
+export default function WoodWeUseSection() {
+  return (
+    <div className="bg-[#222021] pt-10 pb-10 text-white">
+      <div className="w-[90%] m-0 m-auto">
+        <div className="text-[32px] m-0 m-auto w-[80%] md:w-[70%] md:text-center md:text-[64px] lg:text-[90px] lg:w-[40%] ">
+          The wood we work with
+        </div>
+        <div className="overflow-x-scroll overflow-visible flex pb-10 mt-10 lg:mt-20">
+          {mockData.map((item, index) => (
+            <div
+              key={index}
+              className={index !== 0 ? 'pl-15 md:pl-30 lg:pl-40' : ''}
+            >
+              <Wood {...item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
